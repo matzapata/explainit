d6529bcbda349757b1b5cfa62bbb28060d80d483
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-04-05 09:59:31 +0000

    storage module

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { RetrievalAugmentedGenerationService } from './services/rag.service';
 import { ChatsService } from './services/chat.service';
-import { StorageService } from '@src/infrastructure/storage/storage.service';
 import { PaymentsModule } from '@src/payments/payments.module';
 import { PrismaModule } from '@src/database/prisma.module';
 import { ChatRepository } from './repositories/chat.repository';
@@ -10,17 +9,23 @@ import { ResourcesService } from './services/resources.service';
 import { ResourcesRepository } from './repositories/resources.repository';
 import { VectorStoreModule } from '@src/infrastructure/vectorstore/vectorstore.module';
 import { LlmModule } from '@src/infrastructure/llm/llm.module';
+import { StorageModule } from '@src/infrastructure/storage/storage.module';
 
 @Module({
   providers: [
     RetrievalAugmentedGenerationService,
     ChatsService,
-    StorageService,
     ChatRepository,
     ResourcesService,
     ResourcesRepository,
   ],
-  imports: [PrismaModule, PaymentsModule, VectorStoreModule, LlmModule],
+  imports: [
+    PrismaModule,
+    PaymentsModule,
+    VectorStoreModule,
+    LlmModule,
+    StorageModule,
+  ],
   controllers: [ChatController],
 })
 export class ChatModule {}
