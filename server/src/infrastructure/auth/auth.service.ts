f8a8cba9cec8e071ba8682246d51a359abcc0f5f
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-04-05 09:47:56 +0000

    auth module

--- server/src/infrastructure/auth/auth.service.ts
+++ server/src/infrastructure/auth/auth.service.ts
@@ -1,47 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as jwt from 'jsonwebtoken';
-import jwksClient from 'jwks-rsa';
+// Select a provider to use
+export { JwksProvider as AuthService } from './providers/jwks.provider';
 
-export interface JwtPayload {
-  id: string;
-  email: string;
-}
-
-@Injectable()
-export class AuthService {
-  constructor(private readonly configService: ConfigService) {}
-
-  async verifyToken(token: string): Promise<JwtPayload | null> {
-    const client = jwksClient({
-      jwksUri: this.configService.get('AUTH_JWKS_URI'),
-    });
-    function getKey(header, callback) {
-      client.getSigningKey(header.kid, function (err, key) {
-        if (err) return callback(err);
-        else if (!key) return callback(new Error('Key not found'));
-        const signingKey = key.getPublicKey();
-        callback(null, signingKey);
-      });
-    }
-
-    const verify = (token: string): Promise<JwtPayload> =>
-      new Promise((resolve, reject) => {
-        jwt.verify(
-          token,
-          getKey,
-          {},
-          function (err, decoded: { sub: string; 'x-hasura-email': string }) {
-            if (err) reject(err);
-            else resolve({ id: decoded.sub, email: decoded['x-hasura-email'] });
-          },
-        );
-      });
-
-    try {
-      return await verify(token);
-    } catch (e) {
-      return null;
-    }
-  }
-}
+// Export types
+export * from './providers/auth.provider';
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,14 +1,14 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './services/users.service';
-import { AuthService } from '../infrastructure/auth/auth.service';
 import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 import { UsersRepository } from './repositories/users.repository';
 import { PrismaModule } from '@src/database/prisma.module';
+import { AuthModule } from '@src/infrastructure/auth/auth.module';
 
 @Module({
-  providers: [UsersService, AuthService, UsersRepository],
-  imports: [PrismaModule],
+  providers: [UsersService, UsersRepository],
+  imports: [PrismaModule, AuthModule],
   controllers: [UsersController],
   exports: [UsersService, UsersRepository],
 })
