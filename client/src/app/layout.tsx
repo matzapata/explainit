d0f157f1dce96f087ae1c49115a944628de179f7
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-04-07 20:08:59 +0000

    client analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -24,6 +24,13 @@ export default async function RootLayout({
       >
         <Providers>{children}</Providers>
         <Toaster />
+
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "65484acb9faa47bb9ea65760673ef182"}'
+        ></script>
       </body>
     </html>
   );
