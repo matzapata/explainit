1d60804071abf2aebe3eb9bef3db9ce3983f139a
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-05-21 19:20:32 +0300

    remove expo section for now

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,12 +7,12 @@ import HeroSection from '@/components/sections/hero-section';
 import Image from 'next/image';
 import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/outline';
 import images from '@/assets/images';
-import ExploreSection from '@/components/sections/explore-section';
-import { chatService } from '@/lib/services/chat-service';
+// import ExploreSection from '@/components/sections/explore-section';
+// import { chatService } from '@/lib/services/chat-service';
 
 
 export default async function Home() {
-  const chats = await chatService.getPublicChats();
+  // const chats = await chatService.getPublicChats();
 
   return (
     <main className="relative">
@@ -43,7 +43,7 @@ export default async function Home() {
 
         <HeroSection />
 
-        <ExploreSection chats={chats} />
+        {/* <ExploreSection chats={chats} /> */}
 
         <FeaturesSection />
 
@@ -54,41 +54,5 @@ export default async function Home() {
 
       <FooterSection />
     </main>
-  );
-}
-
-function ChatCard() {
-  return (
-    <div className="text-white py-6">
-      <p className="hover:underline cursor-pointer">astral.js</p>
-      <p>
-        Astral.js is a lightweight UI toolkit designed to empower JavaScript
-        developers with powerful tools for building modern user interfaces. With
-        Astral.js, you gain access to a comprehensive set of features including
-        context and state management, event-drive
-      </p>
-
-      <div className="flex items-center space-x-3 mt-2">
-        <div className="flex items-center space-x-2">
-          <Image
-            className="rounded object-cover"
-            width={20}
-            height={16}
-            alt="image"
-            src={images.AvatarSticker01}
-          />
-          <p>Astral</p>
-        </div>
-
-        <p className="text-sm">Updated 1 day ago</p>
-
-        <p className="text-sm">•</p>
-
-        <div className="flex items-center space-x-1">
-          <ChatBubbleOvalLeftEllipsisIcon className="h-4 w-4" />
-          <p className="text-sm">345</p>
-        </div>
-      </div>
-    </div>
   );
 }
