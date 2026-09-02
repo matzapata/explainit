294fb5031b3be7ad563e0ff394f73ebc38b921b3
Matias Zapata <matuzapata@gmail.com>
2026-09-02 13:14:06 +0500

    chore(server): update TypeScript configuration and add build info file
    Changed module resolution from 'nodenext' to 'commonjs', added 'rootDir' and 'ignoreDeprecations' options in tsconfig.json. Introduced a new tsbuildinfo file for TypeScript build information.

--- apps/server/src/infra/vector-store/vectorstore.service.ts
+++ apps/server/src/infra/vector-store/vectorstore.service.ts
@@ -1,5 +1,5 @@
 // Select the vector store provider to use
-export { PrismaVectorStoreProvider as VectorStoreService } from './providers/prismavectorstore.provider';
+export { PrismaVectorStoreProvider as VectorStoreService } from './providers/prisma-vectorstore.provider';
 
 // Export types
 export * from './providers/vectorstore.provider';
